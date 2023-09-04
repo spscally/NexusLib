327d9127746866a6af22a95d96a6bbfd03a6383e
Silent <boksic420@gmail.com>
2023-09-03 21:16:48 -0700

    yeet cooldown util and update plugin utils

--- src/main/java/tsp/nexuslib/util/PluginUtils.java
+++ src/main/java/tsp/nexuslib/util/PluginUtils.java
@@ -3,15 +3,19 @@ package tsp.nexuslib.util;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.PluginCommandYamlParser;
+import org.bukkit.plugin.InvalidDescriptionException;
+import org.bukkit.plugin.InvalidPluginException;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Consumer;
 
 public class PluginUtils {
@@ -40,6 +44,23 @@ public class PluginUtils {
 
     public static List<Command> getCommands(Plugin plugin) {
         return PluginCommandYamlParser.parse(plugin);
+    }
+
+    public static Optional<Plugin> load(File file) {
+        try {
+            return Optional.ofNullable(Bukkit.getPluginManager().loadPlugin(file));
+        } catch (InvalidPluginException | InvalidDescriptionException e) {
+            //throw new RuntimeException(e);
+            return Optional.empty();
+        }
+    }
+
+    public static void disable(Plugin plugin) {
+        Bukkit.getPluginManager().disablePlugin(plugin);
+    }
+
+    public static void enable(Plugin plugin) {
+        Bukkit.getPluginManager().enablePlugin(plugin);
     }
 
 }
